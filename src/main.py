9c47cf83feaef21f3a6cba1352d6d4f707fe8bb5
levi106 <16788901+levi106@users.noreply.github.com>
2023-07-04 09:59:52 +0900

    use session

--- src/main.py
+++ src/main.py
@@ -11,18 +11,19 @@ from opencensus.trace.tracer import Tracer
 REQUEST_URL = str(os.environ.get('REQUEST_URL'))
 REQUEST_INTERVAL_SEC = int(os.environ.get('REQUEST_INTERVAL_SEC', 10))
 
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('').addHandler(AzureLogHandler())
 logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler())
-logger.setLevel(logging.INFO)
 config_integration.trace_integrations(['requests'])
 tracer = Tracer(exporter=AzureExporter(), sampler=ProbabilitySampler(1.0))
 
 def main() -> None:
     logger.info(f'Request URL: {REQUEST_URL}')
     logger.info(f'Interval: {REQUEST_INTERVAL_SEC} sec')
+    s = requests.Session()
     while True:
         with tracer.span(name='main'):
-            res = requests.get(url=REQUEST_URL)
+            res = s.get(url=REQUEST_URL)
             logger.info(f'{res.status_code}')
         time.sleep(REQUEST_INTERVAL_SEC) 
 
